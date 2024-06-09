f3585c6a573dfc969c458ee4f7968192a1b2cb39
Mike <ma7801@gmail.com>
2024-06-08 17:31:48 -0700

    Autostart working

--- startup.sh
+++ startup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Source the user's profile to ensure the environment is correct
-source /home/spotty/.profile
-
 date >> startup.sh.log
 echo "Starting bellmusic startup script..." >> startup.sh.log
+
+# Source the user's profile to ensure the environment is correct
+echo "Loading profile for user spotty..."
+source /home/spotty/.profile
 
 # Open terminal and run librespot
 echo "Opening terminal window and running librespot..." >> startup.sh.log
 lxterminal -e 'bash -c "librespot -u \"ma7801\" -p \"A5AprzD1gWQ2yoXqDTsP\" --B \"pulseaudio\""' &
+
+# Change working directory (server.js must run from bellmusic dir to work)
+echo "Changing directory to /home/spotty/bellmusic"
+cd /home/spotty/bellmusic
 
 # Open terminal and run bellmusic server
 echo "Opening terminal window and running bellmusic server..." >> startup.sh.log
